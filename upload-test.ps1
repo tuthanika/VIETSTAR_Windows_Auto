fdc19c4c3441685b4eae7e34e119a83061746c0e
tuthanika <tuthanika@gmail.com>
2025-11-25 16:06:17 +0700

    new test upload

---
+++ upload-test.ps1
@@ -0,0 +1,22 @@
+param(
+    [string[]]$Files
+)
+
+Write-Host "Starting upload of files: $Files to $env:RCLONE_PATH..."
+
+# Đường dẫn rclone.exe theo biến SCRIPT_PATH trong YAML
+$exePath = Join-Path $env:SCRIPT_PATH "rclone.exe"
+
+foreach ($file in $Files) {
+    $arguments = @(
+        "copy"
+        $file
+        $env:RCLONE_PATH
+        "--config", $env:RCLONE_CONFIG_PATH
+        "--progress"
+    )
+
+    Write-Host "Uploading $file..."
+    Start-Process -FilePath $exePath -ArgumentList $arguments -NoNewWindow -Wait
+    Write-Host "Finished uploading $file."
+}
